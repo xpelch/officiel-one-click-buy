2b9f7a41284d02ecb13251e5ea27d4264b7208c7
xpelch <77986237+xpelch@users.noreply.github.com>
2023-09-18 18:42:31 -0400

    Initial commit

---
+++ src/test/java/ca/ulaval/glo4002/mockexercise/StartByTestingThisTest.java
@@ -0,0 +1,13 @@
+package ca.ulaval.glo4002.mockexercise;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class StartByTestingThisTest {
+
+    private StartByTestingThis service;
+
+    @BeforeEach
+    public void setUp() {
+        service = new StartByTestingThis();
+    }
+}
